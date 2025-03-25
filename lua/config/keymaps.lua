40ba95a2b4063b9a7b5cfa0b4f91dd31dc211ac5
Jiri Fiala <jirik.fiala@post.cz>
2025-03-25 13:58:37 +0100

    feat: initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+-- local Util = require("lazyvim.util")
+
+-- Bordered terminal
+-- vim.keymap.set("n", "<C-/>", function()
+-- Util.terminal(nil, { border = "none" })
+-- end, { desc = "Term with border" })
+
+-- Navigate vim panes better
+--vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+--vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+--vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+--vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Split windows
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Tabs
+keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
