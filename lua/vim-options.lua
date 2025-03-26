175760ecbabf223e8844533ac4d14c670d383fb3
Jiri Fiala <jirik.fiala@post.cz>
2025-03-26 15:16:14 +0100

    Added copilot and two git plugins

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,5 +2,26 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
-vim.cmd("set number")
+vim.g.have_nerd_font = true
+vim.opt.number = true
+
+vim.opt.showmode = false
+
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus"
+end)
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
