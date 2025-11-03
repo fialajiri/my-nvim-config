52b24e08a560b64292861e6918df2093d8dad0c4
Jiri Fiala <jirik.fiala@post.cz>
2025-11-03 09:36:57 +0100

    added github copilot and fix completions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,23 +1,4 @@
 return {
-	"zbirenbaum/copilot.lua",
-	lazy = false, -- Load immediately
-	priority = 1000, -- Load before other plugins
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				enabled = true,
-				keymap = {
-					accept = "<Tab>",
-					accept_word = false,
-					accept_line = false,
-					next = "<C-j>",
-					prev = "<C-k>",
-					dismiss = "<C-d>",
-				},
-			},
-			panel = {
-				enabled = true,
-			},
-		})
-	end,
+	"github/copilot.vim",
+	event = "InsertEnter",
 }
